e05e91ca32078121fe3ba8c5bde5520e1524adc7
vbloher <vbloher7@gmail.com>
2024-12-27 09:28:08 +0100

    add turing testnet

--- src/networkData.ts
+++ src/networkData.ts
@@ -16,18 +16,18 @@ export interface NetworkData {
   balanceCap: number;
 }
 
-const avail: NetworkData = {
+const turing: NetworkData = {
   balanceCap: 100,
-  chains: [{ name: "Avail", id: -1 }],
+  chains: [{ name: "Avail Turing Testnet", id: -1 }],
   currency: "AVL",
   decimals: 18,
   dripAmount: "1",
-  explorer: "https://goldberg.avail.tools/",
+  explorer: "https://avail-turing.subscan.io/",
   networkName: "Avail-Testnet",
-  rpcEndpoint: "wss://rpc-goldberg.sandbox.avail.tools",
+  rpcEndpoint: "wss://turing-rpc.avail.so/ws",
 };
 
-export const networks: Record<string, NetworkData> = { avail };
+export const networks: Record<string, NetworkData> = { turing };
 
 export function getNetworkData(networkName: string): NetworkData {
   if (!Object.keys(networks).includes(networkName)) {
@@ -35,7 +35,7 @@ export function getNetworkData(networkName: string): NetworkData {
       `Unknown NETWORK in env: ${networkName}; valid networks are: [${Object.keys(networks).join(", ")}]`,
     );
   }
-  // networkName value is valdated one line before, safe to use as index
+  // networkName value is validated one line before, safe to use as index
   // eslint-disable-next-line security/detect-object-injection
   return networks[networkName] as NetworkData;
 }
